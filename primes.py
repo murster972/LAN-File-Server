6f76b86e5c41632fca3ec3a795499b9c503ef5e3
murster972 <murster972@yahoo.com>
2016-11-13 01:09:03 +0000

    commitment

--- primes.py
+++ primes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+ #!/usr/bin/env python3
 from random import randint
 import cProfile
 import secrets
@@ -22,35 +22,39 @@ TOFIX: taking between 11s-15s to gen 2*2048 bit prime, find a way
 #TODO: only generate safe primes which satisfy certain condtions,
 #      so that the generator for Diffie Hellman can be secure and
 #      2.
+
+prime_test_sieve = []
+
 def generate_safe_prime(l, u):
     assert l > 1 and l <= u
 
-    #generate 1 256-bit to be used for q
+    #generate 256-bit to be used for q
     #p = Nq + 1
-    q = 0
-
     r = 100 * math.floor(math.log2(2**256) + 1)
 
     while True:
         assert r > 0
         r -= 1
-        n = secrets.randbits(256)
-        if is_prime(n):
-            q = n
-            break
+        #TODO: Figure out why it doesnt alway gen exactly 256-bits
+        q = secrets.randbits(256)
+        if is_prime(q): break
 
     #generate prime p, p = Nq + 1
     p = 0
     while True:
         #TODO: CHANGE TO "CRYPTO SECURE" random number generator
         #TODO: Figure out how to calc n to get N-bit prime
-        #currently returns a 2048-bit prime p.
-        n = randint(2**1791, 2**1792)
+        #? N-bits: N - 256 - 1
+        #n = secrets.randbelow(2**1792 - 2**1791) + 2**1791
+        #n = n = secrets.randbits(1793)
+        n = n = secrets.randbits(1792)
         p = (n * q) + 1
         if is_prime(p): break
-    return p
-    #print(p)
-    #print(math.floor(math.log2(p) + 1))
+    #return p
+    print(p)
+    print(math.floor(math.log2(p) + 1))
+    print(q)
+    print(math.floor(math.log2(q) + 1))
 
 """
 Uses the sieve of eratosthenes to generate a small list of primes, which
@@ -83,28 +87,12 @@ def sieve_of_eratosthenes(n):
     return primes
 
 def is_prime(n):
-    #primes = sieve_of_eratosthenes(2**20)
+    #checks if sieve has already been computed
+    global prime_test_sieve
+    if not prime_test_sieve:
+        prime_test_sieve = sieve_of_eratosthenes(1000)
 
-    """x = 1000
-    flags = {x:1 for x in range(3, x + 1, 2)}
-    flags[2] = 1
-
-    i = 3
-    while i**2 <= x:
-        #mark all multiples of i as composites, i.e set flags to zero
-        for j in range(2, math.floor(x / i) + 1):
-            flags[j * i] = 0
-
-        i += 2
-        while flags[i] != 1:
-            i += 2
-
-    for i in flags:
-        if flags[i] == 1 and n % i == 0:
-            return i == n"""
-    primes = sieve_of_eratosthenes(1000)
-
-    for i in primes:
+    for i in prime_test_sieve:
         if n % i == 0:
             return i == n
     return rabin_miller(n)
@@ -145,7 +133,7 @@ def rabin_miller(n):
         k += 2
     return True
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     x = time.time()
     #print(sieve_of_eratosthenes(1000))
     print(generate_safe_prime(2**2047, 2**2048))
